a5b3111b88770cf89617c0a56425b5b2dbf50fa3
Suley Suárez <yaritzasuarez.803@gmail.com>
2026-06-01 22:56:44 -0500

    Revert "initial migrations"

--- app/models/inventario.py
+++ app/models/inventario.py
@@ -62,7 +62,7 @@ class MovimientoInventario(Base):
 
     id_orden_compra = Column(
         Integer,
-        ForeignKey("pos.orden_compra.id_orden_compra"),
+        ForeignKey("pos.orden_compra.id_orden"),
         nullable=True,
     )
 
@@ -103,10 +103,10 @@ class Alerta(Base):
     cantidad_a_pedir = Column(Numeric)
 
     id_orden_compra = Column(
-    Integer,
-    ForeignKey("pos.orden_compra.id_orden_compra"),
-    nullable=True,
-)
+        Integer,
+        ForeignKey("pos.orden_compra.id_orden"),
+        nullable=True,
+    )
 
     fecha_creacion = Column(
         DateTime,
